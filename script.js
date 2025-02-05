cc79e8a60052d950552538350ee6e61a9472af04
geiseesouz <geiseesouz@gmail.com>
2025-02-05 16:02:05 -0400

    json-server

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
+const artistsContainer = document.getElementById('artists-container')
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists`
     fetch(url)
         .then((response) => response.json())
+        .then((result)=> result.filter((element)=>element.name.toLowerCase().includes(searchTerm)))
         .then((result) => displayResults(result))
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
-
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
+    artistsContainer.innerHTML = '';
+    result.forEach(artist => {
+        const artistCardHTML = `
+        <div class="artist-card">
+            <div class="card-img">
+                <img class="artist-img" src="${artist.urlImg}" alt="${artist.name}" />
+                <div class="play">
+                    <span class="fa fa-solid fa-play"></span>
+                </div>
+            </div>
+            <div class="card-text">
+                <a title="${artist.name}" class="vst" href="#"></a>
+                <span class="artist-name">${artist.name}</span>
+                <span class="artist-categorie">${artist.genre}</span>
+            </div>
+        </div>
+    `;
+    artistsContainer.innerHTML += artistCardHTML;
     });
-
-    resultArtist.classList.remove('hidden');
 }
 
 document.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
+    const searchTerm = searchInput.value.toLowerCase().trim();
+    if(searchTerm == ''){
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
+    }else{
         resultPlaylist.classList.add('hidden');
         resultArtist.classList.remove('hidden');
-        return
+        requestApi(searchTerm);
     }
+    // if (searchTerm === '') {
+    //     resultPlaylist.classList.add('hidden');
+    //     resultArtist.classList.remove('hidden');
+    //     return
+    // }
     
-    requestApi(searchTerm);
 })
